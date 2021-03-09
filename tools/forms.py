a7a2d8c7aed508fd196166ff298a54ccf0d7f858
Olly de Vulder <me@olly.network>
2021-03-09 14:45:41 +0000

    Form styling

--- tools/forms.py
+++ tools/forms.py
@@ -3,11 +3,8 @@ from django import forms
 from . import models
 
 
-class BirthdayForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['password'].widget.attrs['placeholder'] = 'password'
-        
-    class Meta:
-        model = models.Birthday
-        fields = ['password']
+class BirthdayForm(forms.Form):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'password'
+        })
+    )
