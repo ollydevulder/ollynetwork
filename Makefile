f81538247f4b37c64f60f3ba36994e72c23a84ab
Olly de Vulder <me@olly.network>
2021-03-09 14:11:16 +0000

    Reset migrations and set default for Birthday model

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 .DEFAULT_GOAL := setup
 
 setup:
-	./manage.py makemigrations
+	./manage.py makemigrations about tools
 	./manage.py migrate
 	./manage.py collectstatic --noinput
 
--- tools/models.py
+++ tools/models.py
@@ -5,7 +5,7 @@ class Birthday(models.Model):
     person = models.CharField(max_length=100, primary_key=True)
     message = models.TextField(max_length=1000)
     # Yes, there is no encryption here: bazinga.
-    password = models.CharField(max_length=100)
+    password = models.CharField(max_length=100, default='password')
 
     def __str__(self):
         return self.person
