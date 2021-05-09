d7ddac47395b9cf48caf6fd61c6e6ab6c75faa11
Olly de Vulder <me@olly.network>
2021-05-09 16:38:00 +0100

    Add copy to clipboard
    This adds a functional "copy to clipboard" feature for the Short view.

--- static/js/short.js
+++ static/js/short.js
@@ -1,6 +1,16 @@
 function copy () {
-    let short = document.getElementById('short');
+    let short = document.getElementById('path');
+    let button = document.getElementById('copy-button');
+
+    short.style.display = "block";
     short.select();
     short.setSelectionRange(0, 100);
     document.execCommand('copy');
+    short.style.display = "none";
+
+    let str = ' 👍'.repeat(3);
+    if (button.textContent.endsWith(str)) {
+        button.textContent = button.textContent.slice(0, -str.length);
+    }
+    button.textContent += ' 👍';
 }
